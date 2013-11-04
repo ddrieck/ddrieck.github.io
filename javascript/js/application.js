2ae489b5708b4f2361f5c12a5056748d2298e1a6
ddrieck <ddrieck@gmail.com>
2013-11-03 20:43:27 -0500

    Lab 9 Work - event handling and DOM manipulation

--- javascript/js/application.js
+++ javascript/js/application.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
 	alert("Page Loaded!");
-	$(".product").find("img").fadeOut(2000);
-})
+	//$(".product").find("img").fadeOut(2000);
+	$(".details").on("click", function(){
+		$(this).siblings().show();
+	});
+	$(".eu").hide();
+	$(".eu-toggle").on("click", function(){
+		$(".eu").show();
+		$(".us").hide();
+	})
+	$(".us-toggle").on("click", function(){
+		$(".us").show();
+		$(".eu").hide();
+	})
+});
