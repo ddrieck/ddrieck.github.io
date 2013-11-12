017c87bc7f2c82159b5c7b426af9e11f4d745e10
ddrieck <ddrieck@gmail.com>
2013-11-12 13:55:55 -0500

    Lab 10: Added code to calculate total price

--- javascript/js/application.js
+++ javascript/js/application.js
@@ -12,7 +12,46 @@ $(document).ready(function(){
 		$(".us").show();
 		$(".eu").hide();
 	})
-	$(".addtocart").on("click", function(){
-		alert("Thanks for adding the shirt.")
-	})
+
+//Javascript code for cart logic
+var cart = [];
+
+$(".addtocart").on("click", function(){
+	var productClass = $(this).closest(".product");
+	var nameOfProduct = productClass.find("h2").text();
+	var skuOfProduct = productClass.data("sku");	
+	if ($(".us").is(":visible")){
+		var priceOfProduct = productClass.find(".us").text();
+	} else {
+		var priceOfProduct = productClass.find(".eu").text();
+	}
+
+	priceOfProduct = priceOfProduct.slice(1,3);
+	priceOfProduct = parseFloat(priceOfProduct);
+	
+	var cartItem = { name: nameOfProduct,
+                 	sku: skuOfProduct, 
+                 	price: priceOfProduct, 
+                 	quantity: 1 };
+
+	cart.push(cartItem);
+
+	function refreshCart(){
+		total = 0;
+		for(var i = 0; i < cart.length; i++) {
+	 			var cartItem = cart[i];
+	 			total += cartItem.price;
+			}
+		}	
+
+	refreshCart(); // we need to write this.*/
+	
+	if ($(".us").is(":visible")){
+		$(".cart-total").text("Total $" + total);
+	} else {
+		$(".cart-total").text("Total \u20AC" + total);
+	}
 });
+
+
+}); //end of jquery
