e33b21da464f4d76dba60c6108c21362c88104b5
ddrieck <ddrieck@gmail.com>
2013-11-18 22:07:29 -0500

    Updating javascript file to fix menu issues.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,15 @@
-
-$(document).ready(function(){
-	$('a.portfoliopics').colorbox({ opacity:0.5 , rel:'portfolio' });
-	$('.inline').colorbox({ width:"90%", height:"80%" , iframe:true });
-
-	function showdiv(appearingDiv, disappearingDiv){
+function showdiv(appearingDiv, disappearingDiv){
 		var appear = document.getElementById(appearingDiv);
 		var disappear = document.getElementById(disappearingDiv);
 		
 	    		disappear.style.display = "none";
 				appear.style.display = "block";
-	}
-
-	
+		}	
+		
+$(document).ready(function(){
+	$('a.portfoliopics').colorbox({ opacity:0.5 , rel:'portfolio' });
+	$('.inline').colorbox({ width:"90%", height:"80%" , iframe:true });
 
 	});
-	
-
 	
 
